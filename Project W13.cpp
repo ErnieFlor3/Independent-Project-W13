130b2af65950e81a707b2ef90d6f56a774dd8277
ErnestoFlores <156852252+ErnieFlor3@users.noreply.github.com>
2025-05-21 07:14:20 +0000

    project update

---
+++ Project W13.cpp
@@ -0,0 +1,136 @@
+# include <iostream>
+# include <cstdlib>
+# include <ctime>
+# include <string>
+# include <limits>
+using namespace std;
+
+const int MAX_LEVEL = 6;
+const float P = 0.5;
+
+struct Node {
+    string stockSymbol;
+    float price;
+    Node** forward;
+
+    Node(string key, float value, int level) {
+        stockSymbol = key;
+        price = value;
+        forward = new Node*[level + 1];
+        for (int i = 0; i <= level; i++)
+            forward[i] = nullptr;
+    }
+};
+
+class SkipList {
+    private:
+        int level;
+        Node* header;
+
+    public:
+        SkipList() {
+            level = 0;
+            header = new Node(" ", 0.0, MAX_LEVEL);
+            srand(time(0));
+        }
+        int randomlevel() {
+            int lvl = 0;
+            while (((float)rand() / RAND_MAX) < P && lvl < MAX_LEVEL)
+                lvl++;
+                return lvl;
+        }
+        void insert(string symbol, float price) {
+            Node* update[MAX_LEVEL + 1];
+            Node* current = header;
+
+            for (int i = level; i >= 0; i--) {
+                while (current->forward[i] != nullptr && current->forward[i]->stockSymbol < symbol)
+                current = current-> forward[i];
+                update[i] = current;
+            }
+            current = current->forward[0];
+
+            if(current != nullptr && current ->stockSymbol == symbol) {
+                cout << "Updating price for " << symbol << endl;
+                current->price = price;
+            }else {
+                int newLevel = randomlevel();
+                if (newLevel > level) {
+                    for (int i = level + 1; i <= newLevel; i++)
+                        update[i] = header;
+                    level = newLevel;
+                }
+                Node* newNode = new Node(symbol, price, newLevel);
+                for (int i = 0; i <= newLevel; i++) {
+                    newNode->forward[i] = update[i]->forward[i];
+                    update[i]-> forward[i] = newNode;
+                }
+                cout << "Inserted: " << symbol << " at $" << price << endl;
+            }
+        }
+        void search(string symbol) {
+            Node* current = header;
+            for (int i = level; i >= 0; i--) {
+                while (current -> forward[i] != nullptr && current->forward[i]->stockSymbol < symbol)
+                current = current->forward[i];
+
+            }
+            current = current->forward[0];
+            if (current != nullptr && current-> stockSymbol == symbol) {
+                cout << symbol << " found with price: $" << current->price << endl;
+
+            }else {
+                cout << symbol << "not found." << endl;
+            }
+        }
+        void display() {
+            cout << "\nStock Prices (level 0):\n";
+            Node* node = header-> forward[0];
+            while(node != nullptr) {
+                cout << node->stockSymbol << " $" << node -> price << endl;
+                node = node->forward[0];
+            }
+        }
+};
+
+int main () {
+    SkipList stockPrices;
+    int choice;
+    string symbol;
+    float price;
+
+    do { 
+        cout << "\n--- Stock Ticker Price Index ---\n";
+        cout << "1. Insert or Update Stock Price\n";
+        cout << "2. Search for Stock\n";
+        cout << "3. Display All Stocks\n";
+        cout << "4. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter stock symbol (e.g., AAPL): ";
+                cin >> symbol;
+                cout << "Enter price: ";
+                cin >> price;
+                stockPrices.insert(symbol, price);
+                break;
+            case 2:
+                cout << "Enter stock symbol to search: ";
+                cin >> symbol;
+                stockPrices.search(symbol);
+                break;
+            case 3:
+                stockPrices.display();
+                break;
+            case 4:
+                cout << "Exiting...\n";
+                break;
+            default:
+                cout << "Invalid choice.\n";
+        }
+    }while (choice != 4);
+    return 0;
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Independent-Project-W13
+Project is a Stock Ticker Price Index WITH SkipList implementation
+
+Description
+This project implements a skip list to maintain a real-time stock ticker system. It provides users with the ability to 
+    Insert or update stock prices
+    Search for a stock Symbol
+    Display all current stock prices in sorted order
+The SkipLists used in this project are meant to have a faster than average result for search, insert, and delete operations, which ultimately makes it suitable for time-sensitive systems like trading platforms. 
+
+To run this program make sure compiller is installed
+    g++ -std=c++11 -o ProjectW13 Project\ W13.cpp
+
+TO RUN USE
+    ./project
+
+some other features include 
+    Menu focused CLI interface
+    Fast insert, search, and display using skip list
+    Designed for possible real world application in finance 
